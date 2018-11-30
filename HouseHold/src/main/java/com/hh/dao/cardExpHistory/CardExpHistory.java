cc1504644a7114a523a1a6dad0753acef1cc1750
hilu0318 <byunghakjang1230@gmail.com>
2018-12-01 01:42:06 +0900

    서비스 등록

--- HouseHold/src/main/java/com/hh/dao/cardExpHistory/CardExpHistory.java
+++ HouseHold/src/main/java/com/hh/dao/cardExpHistory/CardExpHistory.java
@@ -1,5 +1,26 @@
 package com.hh.dao.cardExpHistory;
 
+import com.hh.domain.HData;
+
 public interface CardExpHistory {
 	
+	/* ---------------
+	 * 카드지출내역기본조회
+	 * */
+	public HData iqryCardExpHistoryBasic(HData iData);
+	
+	/* ---------------
+	 * 카드지출내역기본등록
+	 * */
+	public void rgstCardExpHistoryBasic(HData iData);
+	
+	/* ---------------
+	 * 카드지출내역기본수정
+	 * */
+	public void updtCardExpHistoryBasic(HData iData);
+	
+	/* ---------------
+	 * 카드지출내역삭제여부수정
+	 * */
+	public void updtDelYnCardExpHistory(HData iData);
 }
--- HouseHold/src/main/java/com/hh/dao/cardExpHistory/CardExpHistoryImpl.java
+++ HouseHold/src/main/java/com/hh/dao/cardExpHistory/CardExpHistoryImpl.java
@@ -1,8 +1,56 @@
 package com.hh.dao.cardExpHistory;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.hh.domain.HData;
 
 @Repository
 public class CardExpHistoryImpl implements CardExpHistory {
+
+	/* 기준 테이블 
+	 * CRD_CRD02
+	 * */
+	@Inject
+	private SqlSession session;
+	
+	/* ---------------
+	 * 카드지출내역기본조회
+	 * */
+	@Override
+	public HData iqryCardExpHistoryBasic(HData iData) {
+		// TODO Auto-generated method stub
+		System.out.println(iData.toString());
+		return session.selectOne("CRD_CRD02.iqryCardExpHistoryBasic", iData);
+	}
+	
+	/* ---------------
+	 * 카드지출내역기본등록
+	 * */
+	@Override
+	public void rgstCardExpHistoryBasic(HData iData) {
+		// TODO Auto-generated method stub
+		session.insert("CRD_CRD02.rgstCardExpHistoryBasic", iData);
+	}
+
+	/* ---------------
+	 * 카드지출내역기본수정
+	 * */
+	@Override
+	public void updtCardExpHistoryBasic(HData iData) {
+		// TODO Auto-generated method stub
+		session.update("CRD_CRD02.updtCardExpHistoryBasic", iData);
+	}
+
+	/* ---------------
+	 * 카드지출내역삭제여부수정
+	 * */
+	@Override
+	public void updtDelYnCardExpHistory(HData iData) {
+		// TODO Auto-generated method stub
+		session.update("CRD_CRD02.updtDelYnCardExpHistory", iData);
+	}
 
 }
